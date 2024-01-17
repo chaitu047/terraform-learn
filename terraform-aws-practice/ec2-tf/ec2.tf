7d362e189b8b2d9eb4c96e0cbede88596f4a72ee
chaitu047 <chaitu.klk047@gmail.com>
2024-01-17 18:47:39 +0530

    ec2-fix-working

--- terraform-aws-practice/ec2-tf/ec2.tf
+++ terraform-aws-practice/ec2-tf/ec2.tf
@@ -1,7 +1,21 @@
+data "aws_vpc" "selected" {
+  default = false
+}
+
+data "aws_security_group" "selected" {
+  id = "sg-09958aae927b9611c"
+}
+
+data "aws_subnet" "public" {
+  id = "subnet-0eaad7feee733697b"
+}
+
 resource "aws_instance" "web" {
-  ami               = var.ami_id
-  instance_type     = "t3.micro"
-  availability_zone = "us-east-1a"
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
+  availability_zone      = "us-east-1a"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+  subnet_id              = data.aws_subnet.public.id
   tags = {
     Name = "HelloWorld"
   }
